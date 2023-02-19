b6d874f634df2eb351373d6c124f7da9599b93fd
YhgzXxfz <qingfeiyou@gmail.com>
2023-02-19 13:35:29 -0800

    feat(nanogpt): add feed forward layer

--- nanogpt/bigram.py
+++ nanogpt/bigram.py
@@ -100,6 +100,15 @@ class MultiHeadAttention(nn.Module):
         return torch.cat([h(x) for h in self.heads], dim=-1)
 
 
+class FeedForward(nn.Module):
+    def __init__(self, n_embed) -> None:
+        super().__init__()
+        self.net = nn.Sequential(nn.Linear(n_embed, n_embed), nn.ReLU())
+
+    def forward(self, x):
+        return self.net(x)
+
+
 # model
 class BiGramLanguageModel(nn.Module):
     def __init__(self) -> None:
@@ -108,6 +117,7 @@ class BiGramLanguageModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
         self.sa_head = MultiHeadAttention(num_heads=4, head_size=n_embed // 4)
+        self.ffw = FeedForward(n_embed)
         self.lm_head = nn.Linear(n_embed, vocab_size)
 
     def forward(self, idx, targets=None):  # idx is (B, T)
@@ -117,6 +127,7 @@ class BiGramLanguageModel(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # (T, C)
         x = tok_emb + pos_emb  # (B, T, C)
         x = self.sa_head(x)
+        x = self.ffw(x)
         logits = self.lm_head(x)  # (B, T, vocab_size)
 
         if targets is None:
